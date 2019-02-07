9b33c51ca27f3d268e09d4a0022b6274cd2e1c1f
unknown <holycowmamitas@yahoo.com>
2019-02-07 15:06:25 -0700

    Add Box and OnBoxTrigger enter scripts

---
+++ BoxGame/Assets/Scripts/Box.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private bool follow = false;
+
+    void FixedUpdate()
+    {
+        if (follow)
+        {
+            print("Following");
+        }
+    }
+
+    public void SetFollow(bool set)
+    {
+        follow = set;
+    }
+}
---
+++ BoxGame/Assets/Scripts/OnBoxTriggerEnter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBoxTriggerEnter : MonoBehaviour
+{
+    [SerializeField] private Box box;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent != null && other.transform.parent.name.Equals("Character"))
+        {
+            box.SetFollow(true);
+        }
+    }
+}
